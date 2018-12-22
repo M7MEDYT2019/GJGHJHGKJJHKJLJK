8f74b8b356670a199f347959d2e439beb5f160c4
Mohamed <42390271+M7MEDYT2019@users.noreply.github.com>
2018-12-22 21:42:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', function(){
     var ms = 10000 ;
-    var setGame = [' %help','GaMerZ Community','Have Fun !',' Iam Here For Games',' %help'];
+    var setGame = [' %help','Venom Security','Have Fun !',' Iam Here For Games',' %help'];
     var i = -1;
     var j = 0;
     setInterval(function (){
